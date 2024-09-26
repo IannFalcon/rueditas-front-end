97d2bdcecd686f971cd640a3b9cceeabd6c579da
Iann <falconcolos@gmail.com>
2024-09-26 18:57:09 -0500

    Creacion del proyecto

---
+++ src/main/java/pe/edu/cibertec/rueditas_front_end/RueditasFrontEndApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.rueditas_front_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RueditasFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RueditasFrontEndApplication.class, args);
+	}
+
+}
