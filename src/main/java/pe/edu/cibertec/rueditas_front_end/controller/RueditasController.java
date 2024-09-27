6e5daacc3548375fef30c840176119f9ba603e80
Iann <falconcolos@gmail.com>
2024-09-26 20:44:58 -0500

    Se implementaron validaciones y una interface para mostrar los datos del vehiculo.
    - Se implementaron validaciones para que la placa ingresada no sea nula y cumpla con el formato de placa XXX-XXX.
    - Se implemento la interface que mostrara los datos del vehiculo.

--- src/main/java/pe/edu/cibertec/rueditas_front_end/controller/RueditasController.java
+++ src/main/java/pe/edu/cibertec/rueditas_front_end/controller/RueditasController.java
@@ -3,7 +3,9 @@ package pe.edu.cibertec.rueditas_front_end.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import pe.edu.cibertec.rueditas_front_end.viewmodels.BuscarVehiculoModel;
 
 @Controller
@@ -15,6 +17,38 @@ public class RueditasController {
         BuscarVehiculoModel buscarVehiculoModel = new BuscarVehiculoModel("00", "");
         model.addAttribute("buscarVehiculoModel", buscarVehiculoModel);
         return "inicio";
+    }
+
+    @PostMapping("/buscar-vehiculo")
+    public String buscarVehiculo (@RequestParam("placa") String placa, Model model) {
+
+        // Si no se ingresó una placa
+        if (placa == null || placa.trim().isEmpty()) {
+
+            // Retornamos un mensaje y redirigimos al inicio
+            BuscarVehiculoModel buscarVehiculoModel = new BuscarVehiculoModel("01", "Error: Debe ingresar una placa.");
+            model.addAttribute("buscarVehiculoModel", buscarVehiculoModel);
+            return "inicio";
+
+        }
+
+        // Creamos una variable que contenga la validación del formato de la placa
+        String validarPlaca = "^[A-Z0-9]{3}-[A-Z0-9]{3}$";
+
+        // Si lo placa no cumple con el formato enviado
+        if (!placa.matches(validarPlaca)) {
+
+            // Retornamos un mensaje y redirigimos al inicio
+            BuscarVehiculoModel buscarVehiculoModel = new BuscarVehiculoModel("02", "Error: El formato de la placa debe ser XXX-XXX.");
+            model.addAttribute("buscarVehiculoModel", buscarVehiculoModel);
+            return "inicio";
+
+        }
+
+        BuscarVehiculoModel buscarVehiculoModel = new BuscarVehiculoModel("00", "La placa ingresada es válida.");
+        model.addAttribute("buscarVehiculoModel", buscarVehiculoModel);
+        return "resultados";
+
     }
 
 }
